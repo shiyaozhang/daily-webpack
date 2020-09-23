0d7af2dcf4f8d7e6b9c336b4e4b5a8d736c2ce52
zhangshiyao <zhangshiyao@huahuo.cn>
2020-09-23 16:19:22 +0800

    part 16

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry:{
+    index:'./src/index.js',
+    search:'./src/search.js'
+  },
+  output:{
+    filename:'[name].js',
+    path:path.resolve(__dirname,'dist')
+  },
+  mode:'production',
+  module:{
+    rules:[
+      {
+        test:/.js$/,
+        use:'babel-loader'
+      }
+    ]
+  }
+}
